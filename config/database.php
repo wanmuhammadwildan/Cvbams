2b9eaf6978f4c62cdbec33f5fc6b00145e6ba61e
adityauzi <m.adityauzi0276@gmail.com>
2026-02-08 08:10:34 +0700

    Siap online di Vercel

--- config/database.php
+++ config/database.php
@@ -85,8 +85,7 @@ return [
 
         'pgsql' => [
             'driver' => 'pgsql',
-            'url' => env('DB_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'url' => env('DATABASE_URL'), // Tambahkan baris ini agar Neon DB terbaca            'host' => env('DB_HOST', '127.0.0.1'),
             'port' => env('DB_PORT', '5432'),
             'database' => env('DB_DATABASE', 'laravel'),
             'username' => env('DB_USERNAME', 'root'),
