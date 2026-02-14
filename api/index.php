da60042437959bc415bc2fd6752668a953c54359
wanmuhammadwildan <162114297+wanmuhammadwildan@users.noreply.github.com>
2026-02-14 11:08:00 +0700

    Debug: simple hello world in api/index.php to test routing

--- api/index.php
+++ api/index.php
@@ -1,8 +1,3 @@
 <?php
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
-
-putenv('VIEW_COMPILED_PATH=/tmp');
-// api/index.php
-require __DIR__ . '/../public/index.php';
+echo "Hello from Vercel! Your routing is working.";
+die();
