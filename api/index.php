8dda77b93cc62f1840ad127db309cb5ca16fe452
wanmuhammadwildan <162114297+wanmuhammadwildan@users.noreply.github.com>
2026-02-14 11:06:03 +0700

    Fix: use vercel-php@0.6.1 and simplify routes for better compatibility

--- api/index.php
+++ api/index.php
@@ -1,4 +1,8 @@
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 putenv('VIEW_COMPILED_PATH=/tmp');
 // api/index.php
 require __DIR__ . '/../public/index.php';
